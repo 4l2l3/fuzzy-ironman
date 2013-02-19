01eea462db30ec762cf17227e959e613caeff66a
4|2|3 <sakatai@gmail.com>
2013-02-18 22:10:14 -0500

    plain app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webcomic::Application.config.secret_token = 'b5f6d3368862153a4f85199dbe346c3af17f2f1116c6f7f08ecdc11a51b6a816954207a8f24efee18b5280eb7c553a0abd9e17f279743ac72a77a17ff855d959'
